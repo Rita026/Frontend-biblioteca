9f910c91fa4896a66cdc03a234b168ec76f1f443
IRVIN ISAEL MARTINEZ ALEJO <irvinmartinealejo@gmail.com>
2026-02-27 21:14:13 -0600

    Refactor RegistrarLibro component structure and styles

--- src/pages/Libros/RegistrarLibro.tsx
+++ src/pages/Libros/RegistrarLibro.tsx
@@ -1,7 +1,7 @@
 export default function RegistrarLibro() {
   return (
-    <section>
-      <h1>Registrar libro</h1>;
+    <section className="fade-in show p-6">
+      <h1 className="text-2xl font-bold mb-4 fade-in show">Registrar libro</h1>
     </section>
-  )
+  );
 }
