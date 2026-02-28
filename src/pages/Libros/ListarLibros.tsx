84ae963f0fc92a8f030f2c08d3f4cbf8bc4a287c
IRVIN ISAEL MARTINEZ ALEJO <irvinmartinealejo@gmail.com>
2026-02-27 21:13:59 -0600

    Refactor ListarLibros component structure and styles

--- src/pages/Libros/ListarLibros.tsx
+++ src/pages/Libros/ListarLibros.tsx
@@ -1,8 +1,7 @@
 export default function ListarLibros() {
   return (
-    <section>
-       <h1>Listar libros</h1>;
+    <section className="fade-in show p-6">
+      <h1 className="text-2xl font-bold mb-4 fade-in show">Listar libros</h1>
     </section>
-  )
- 
+  );
 }
